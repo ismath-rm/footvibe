3f0f65c99f8ef9a32a8cc089f4c2a2d1f9d65e7c
ismath <ismathrm9@.com>
2024-01-25 17:23:26 +0530

    edit settings

--- footvibe/footvibe/settings.py
+++ footvibe/footvibe/settings.py
@@ -135,9 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 import os
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
 
 
 # media file configuration
