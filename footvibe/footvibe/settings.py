54b10a5c7c33d0315b798eb8a1d5facf355a22de
ismath <ismathrm9@.com>
2024-01-25 18:10:54 +0530

    Static file problem resolve

--- footvibe/footvibe/settings.py
+++ footvibe/footvibe/settings.py
@@ -135,9 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 import os
-STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [
+    "footvibe/static",
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # media file configuration
